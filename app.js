f85fc7a0f207daa667ee7477f85c26cba5074e9c
manuelpezzi <pezzimanuel@gmail.com>
2025-02-10 12:24:07 +0100

    first oggetto creato

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-app.get('/api/bacheca'), (reg, res) => {
+app.use(express.static('public'));
+
+app.get('/'), (reg, res) => {
+    res.send("ciao mondo")
 
 }
+app.get('/bacheca', (req, res) => {
+    const post = [
+        {
+            titolo: 'ciambellone',
+            contenuto: "Si presenta come una torta forata al centro.o con la forma di uno sfilatino di pane. Per prepararlo bisogna mescolare uova, zucchero, latte, fecola di patate, lievito e limone, versare l'amalgama nella teglia da ciambellone, cospargere di zucchero il dolce e infornarlo.La ciambella può essere insaporita con il liquore, il cacao, i canditi o della frutta secca a piacere.",
+            immagine: "/img/ciambellone.jpg",
+            tag: ["dolce", "da forno", "lievitato"]
+
+        }
+
+    ]
+    res.json(post);
+
+})
+app.listen(port, () => {
+    console.log(`example app listening on port ${port}`)
+})
